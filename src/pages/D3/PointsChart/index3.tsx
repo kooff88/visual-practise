709627d2d4776f455578d46f30a58c8a8b10dde9
dapeng <zhangpeng101@163.com>
2020-12-10 09:50:31 +0800

    点图细胞- voronoi

--- src/pages/D3/PointsChart/index3.tsx
+++ src/pages/D3/PointsChart/index3.tsx
@@ -10,6 +10,7 @@ import { translate } from '@antv/g2/lib/util/transform';
 
 const PointsChart: React.FC<{}> = (props) => {
 
+
     useEffect(() => { 
         showPic()
     },[])
@@ -39,47 +40,48 @@ const PointsChart: React.FC<{}> = (props) => {
         }))
 
 
-        let voronoi = d3.Delaunay
-            .from(circles, d => d.x, d => d.y)
-            .voronoi([0, 0, width, height])
-
-
-        let circle = chart.append("g")
-            .selectAll('circle')
+        chart.selectAll('circle')
             .data(circles)
             .join('circle')
             .attr('cx', d => d.x)
             .attr('cy', d => d.y)
             .attr('r', radius)
             .attr('fill', d => d3.schemeCategory10[d.index % 10])
+            .call(
+                d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended)
+            )
+            .on("click", clicked);
+        
+        function clicked(event, d) { 
+            if (event.defaultPrevented) return;
 
-        let mesh = chart.append("path")
-            .attr("fill", "none")
-            .attr("stroke", "#ccc")
-            .attr("stroke-width", 1)
-            .attr("d", voronoi.render());
-        
-        let cell = chart.append("g")
-            .attr("fill", "none")
-            .attr("pointer-events", "all")
-            .selectAll("path")
-            .data(circles)
-            .join("path")
-            .attr("d", (d, i) => voronoi.renderCell(i))
-            .call(d3.drag()
-            .on("start", (event, d) => circle.filter(p => p === d).raise().attr("stroke", "black"))
-            .on("drag", (event, d) => (d.x = event.x, d.y = event.y))
-            .on("end", (event, d) => circle.filter(p => p === d).attr("stroke", null))
-            .on("start.update drag.update end.update", update));
-        
-        function update() { 
-            voronoi = d3.Delaunay.from(circles, d => d.x, d => d.y).voronoi([0, 0, width, height]);
-            circle.attr("cx", d => d.x).attr("cy", d => d.y);
-            cell.attr("d", (d, i) => voronoi.renderCell(i));
-            mesh.attr("d", voronoi.render());
+            d3.select(this).transition()
+                .attr("fill", "black")
+                .attr("r", radius * 2)
+                .transition()
+                .attr("r", radius)
+                .attr("fill", d3.schemeCategory10[d.index % 10])
+
+        }
+    
+
+        function dragstarted() { 
+            d3.select(this).attr("stroke", "black")
+        }
+
+        function dragged(event, d) { 
+            d3.select(this)
+                .raise()
+                .attr('cx', d.x = event.x)
+                .attr('cy', d.y = event.y)
         }
         
-        
+        function dragended() { 
+            d3.select(this).attr("stroke", null);
+        }
 
 
     }
